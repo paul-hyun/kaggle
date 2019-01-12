1cdfcc324f6436a4a2743b1ce32f1283d52aad23
paul-hyun <cchyun@gmail.com>
2019-01-13 00:09:06 +0900

    필터링 적용

--- QIQC/src/kaggle_cnn.py
+++ QIQC/src/kaggle_cnn.py
@@ -1,8 +1,9 @@
+import re
 import numpy as np
 import pandas as pd
 
-from keras import backend as K
-from keras.layers import Dense, Embedding, Input, Dropout
+from keras import optimizers
+from keras.layers import Dense, Embedding, Input, Dropout, SpatialDropout1D
 from keras.layers import Conv1D, MaxPooling1D, GlobalMaxPooling1D, Flatten
 from keras.models import Model
 from keras.preprocessing.sequence import pad_sequences
@@ -16,15 +17,92 @@ from sklearn.metrics import f1_score
 # Defind
 #
 DATA_DIR = "../input/"
-NROWS = 100  # read count: None = all
+NROWS = 1000  # read count: None = all
 
-N_EPOCH = 3
+N_EPOCH = 10
 N_BATCH = 1000 if NROWS is None else NROWS // 3
 
 N_EMBED = 300
-MAX_FEATURES = 300000
+MAX_FEATURES = 100000
 MAXLEN = 70
-THRESHOLD = 0.34
+THRESHOLD = 0.33
+LEARN_RATE = 0.001
+
+#
+# Clean Text
+#
+puncts = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&', '/', '[', ']', '>', '%', '=', '#', '*', '+', '\\', '•',  '~', '@', '£', 
+ '·', '_', '{', '}', '©', '^', '®', '`',  '<', '→', '°', '€', '™', '›',  '♥', '←', '×', '§', '″', '′', 'Â', '█', '½', 'à', '…', 
+ '“', '★', '”', '–', '●', 'â', '►', '−', '¢', '²', '¬', '░', '¶', '↑', '±', '¿', '▾', '═', '¦', '║', '―', '¥', '▓', '—', '‹', '─', 
+ '▒', '：', '¼', '⊕', '▼', '▪', '†', '■', '’', '▀', '¨', '▄', '♫', '☆', 'é', '¯', '♦', '¤', '▲', 'è', '¸', '¾', 'Ã', '⋅', '‘', '∞', 
+ '∙', '）', '↓', '、', '│', '（', '»', '，', '♪', '╩', '╚', '³', '・', '╦', '╣', '╔', '╗', '▬', '❤', 'ï', 'Ø', '¹', '≤', '‡', '√', ]
+ 
+def clean_text(x):
+    x = str(x) 
+    for punct in "/-'":
+        x = x.replace(punct, ' ')
+    for punct in '&':
+        x = x.replace(punct, f' {punct} ')
+    for punct in '?!.,"#$%\'()*+-/:;<=>@[\\]^_`{|}~' + '“”’':
+        x = x.replace(punct, '')
+    for punct in puncts:
+        x = x.replace(punct, f' {punct} ')
+    return x
+
+def clean_numbers(x):
+    x = re.sub('[0-9]{5,}', '#####', x)
+    x = re.sub('[0-9]{4,}', '####', x)
+    x = re.sub('[0-9]{3,}', '###', x)
+    x = re.sub('[0-9]{2,}', '##', x)
+    return x
+
+
+#
+# miss spell
+#
+mispell_dict = {"ain't": "is not", "aren't": "are not","can't": "cannot", "'cause": "because", "could've": "could have",
+"couldn't": "could not", "didn't": "did not",  "doesn't": "does not", "don't": "do not", "hadn't": "had not", "hasn't": "has not",
+"haven't": "have not", "he'd": "he would","he'll": "he will", "he's": "he is", "how'd": "how did", "how'd'y": "how do you",
+"how'll": "how will", "how's": "how is",  "I'd": "I would", "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have",
+"I'm": "I am", "I've": "I have", "i'd": "i would", "i'd've": "i would have", "i'll": "i will",  "i'll've": "i will have",
+"i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would", "it'd've": "it would have", "it'll": "it will", 
+"it'll've": "it will have","it's": "it is", "let's": "let us", "ma'am": "madam", "mayn't": "may not", "might've": "might have",
+"mightn't": "might not","mightn't've": "might not have", "must've": "must have", "mustn't": "must not", "mustn't've": "must not have",
+"needn't": "need not", "needn't've": "need not have","o'clock": "of the clock", "oughtn't": "ought not", "oughtn't've": "ought not have",
+"shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have", "she'd": "she would", "she'd've": "she would have",
+"she'll": "she will", "she'll've": "she will have", "she's": "she is", "should've": "should have", "shouldn't": "should not",
+"shouldn't've": "should not have", "so've": "so have","so's": "so as", "this's": "this is","that'd": "that would",
+"that'd've": "that would have", "that's": "that is", "there'd": "there would", "there'd've": "there would have",
+"there's": "there is", "here's": "here is","they'd": "they would", "they'd've": "they would have", "they'll": "they will",
+"they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have", "wasn't": "was not",
+"we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have", "we're": "we are",
+"we've": "we have", "weren't": "were not", "what'll": "what will", "what'll've": "what will have", "what're": "what are",
+"what's": "what is", "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did",
+"where's": "where is", "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is",
+"who've": "who have", "why's": "why is", "why've": "why have", "will've": "will have", "won't": "will not", "won't've": "will not have",
+"would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have", "y'all": "you all", "y'all'd": "you all would",
+"y'all'd've": "you all would have","y'all're": "you all are","y'all've": "you all have","you'd": "you would", "you'd've": "you would have",
+"you'll": "you will", "you'll've": "you will have", "you're": "you are", "you've": "you have", 'colour': 'color', 'centre': 'center',
+'favourite': 'favorite', 'travelling': 'traveling', 'counselling': 'counseling', 'theatre': 'theater', 'cancelled': 'canceled',
+'labour': 'labor', 'organisation': 'organization', 'wwii': 'world war 2', 'citicise': 'criticize', 'youtu ': 'youtube ', 'Qoura': 'Quora',
+'sallary': 'salary', 'Whta': 'What', 'narcisist': 'narcissist', 'howdo': 'how do', 'whatare': 'what are', 'howcan': 'how can',
+'howmuch': 'how much', 'howmany': 'how many', 'whydo': 'why do', 'doI': 'do I', 'theBest': 'the best', 'howdoes': 'how does',
+'mastrubation': 'masturbation', 'mastrubate': 'masturbate', "mastrubating": 'masturbating', 'pennis': 'penis', 'Etherium': 'Ethereum',
+'narcissit': 'narcissist', 'bigdata': 'big data', '2k17': '2017', '2k18': '2018', 'qouta': 'quota', 'exboyfriend': 'ex boyfriend',
+'airhostess': 'air hostess', "whst": 'what', 'watsapp': 'whatsapp', 'demonitisation': 'demonetization', 'demonitization': 'demonetization',
+'demonetisation': 'demonetization'}
+
+def _get_mispell(mispell_dict):
+    mispell_re = re.compile('(%s)' % '|'.join(mispell_dict.keys()))
+    return mispell_dict, mispell_re
+
+mispellings, mispellings_re = _get_mispell(mispell_dict)
+
+def replace_typical_misspell(text):
+    def replace(match):
+        return mispellings[match.group(0)]
+    return mispellings_re.sub(replace, text)
+
 
 #
 # load data from train.csv, test.csv
@@ -34,13 +112,24 @@ def load_data():
     train_df = pd.read_csv(DATA_DIR + "train.csv", nrows=NROWS)
     test_df = pd.read_csv(DATA_DIR + "test.csv", nrows=NROWS)
 
+    for df in [train_df, test_df]:
+        df["question_text"] = df["question_text"].str.lower()
+        df["question_text"] = df["question_text"].apply(lambda x: clean_text(x))
+        df["question_text"] = df["question_text"].apply(lambda x: clean_numbers(x))
+        df["question_text"] = df["question_text"].apply(lambda x: replace_typical_misspell(x))
+        df["question_text"].fillna("_##_", inplace=True)
+
     # fill up the values
-    train_X = train_df["question_text"].fillna("_##_").values
-    test_X = test_df["question_text"].fillna("_##_").values
+    train_X = train_df["question_text"].values
+    test_X = test_df["question_text"].values
 
     # Tokenize the sentences
-    tokenizer = Tokenizer(num_words=MAX_FEATURES)
-    tokenizer.fit_on_texts(list(train_X))
+    tokenizer = Tokenizer(num_words=min(MAX_FEATURES, NROWS + 1))
+    print(min(MAX_FEATURES, NROWS + 1))
+    all_Text = []
+    all_Text.extend(train_X)
+    # all_Text.extend(test_X)
+    tokenizer.fit_on_texts(list(all_Text))
     train_X = tokenizer.texts_to_sequences(train_X)
     test_X = tokenizer.texts_to_sequences(test_X)
 
@@ -52,31 +141,100 @@ def load_data():
     train_y = train_df['target'].values
     test_id = test_df["qid"].values
 
-    train_X, valid_X, train_y, valid_y = train_test_split(train_X, train_y, test_size=0.05, random_state=42)
+    train_X, valid_X, train_y, valid_y = train_test_split(train_X, train_y, test_size=0.1, random_state=42, stratify=train_y)
     return train_X, valid_X, test_X, train_y, valid_y, tokenizer.word_index, test_id
+
+
+#
+# embedding
+#
+def load_glove(word_index):
+    EMBEDDING_FILE = '../input/embeddings/glove.840B.300d/glove.840B.300d.txt'
+    def get_coefs(word,*arr): return word, np.asarray(arr, dtype='float32')
+    embeddings_index = dict(get_coefs(*o.split(" ")) for o in open(EMBEDDING_FILE))
+
+    all_embs = np.stack(embeddings_index.values())
+    emb_mean,emb_std = all_embs.mean(), all_embs.std()
+    embed_size = all_embs.shape[1]
+
+    # word_index = tokenizer.word_index
+    nb_words = min(MAX_FEATURES, len(word_index))
+    embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, embed_size))
+    for word, i in word_index.items():
+        if i >= MAX_FEATURES: continue
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None: embedding_matrix[i] = embedding_vector
+            
+    return embedding_matrix 
+    
+def load_fasttext(word_index):    
+    EMBEDDING_FILE = '../input/embeddings/wiki-news-300d-1M/wiki-news-300d-1M.vec'
+    def get_coefs(word,*arr): return word, np.asarray(arr, dtype='float32')
+    embeddings_index = dict(get_coefs(*o.split(" ")) for o in open(EMBEDDING_FILE) if len(o)>100)
+
+    all_embs = np.stack(embeddings_index.values())
+    emb_mean,emb_std = all_embs.mean(), all_embs.std()
+    embed_size = all_embs.shape[1]
+
+    # word_index = tokenizer.word_index
+    nb_words = min(MAX_FEATURES, len(word_index))
+    embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, embed_size))
+    for word, i in word_index.items():
+        if i >= MAX_FEATURES: continue
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None: embedding_matrix[i] = embedding_vector
+
+    return embedding_matrix
+
+def load_para(word_index):
+    EMBEDDING_FILE = '../input/embeddings/paragram_300_sl999/paragram_300_sl999.txt'
+    def get_coefs(word,*arr): return word, np.asarray(arr, dtype='float32')
+    embeddings_index = dict(get_coefs(*o.split(" ")) for o in open(EMBEDDING_FILE, encoding="utf8", errors='ignore') if len(o)>100)
+
+    all_embs = np.stack(embeddings_index.values())
+    emb_mean,emb_std = all_embs.mean(), all_embs.std()
+    embed_size = all_embs.shape[1]
+
+    # word_index = tokenizer.word_index
+    nb_words = min(MAX_FEATURES, len(word_index))
+    embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, embed_size))
+    for word, i in word_index.items():
+        if i >= MAX_FEATURES: continue
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None: embedding_matrix[i] = embedding_vector
+    
+    return embedding_matrix
 
 
 #
 # build model
 #
-def build_model(vocab_size):
+def build_model(embedding_matrix):
     inp = Input(shape=(MAXLEN,), dtype='float64')
-    input_embedding = Embedding(input_dim=vocab_size+1, output_dim=128, input_length=MAXLEN)(inp)
+    input_embedding = Embedding(input_dim=len(embedding_matrix), output_dim=N_EMBED, weights=[embedding_matrix], input_length=MAXLEN)(inp)
     
-    drop1 = Dropout(0.2)(input_embedding)
+    drop1 = SpatialDropout1D(0.2)(input_embedding)
     conv1 = Conv1D(128, 3, activation='relu')(drop1)
-#     pool1 = MaxPooling1D(3)(conv1)
-#     conv2 = Conv1D(128, 3, activation='relu')(pool1)
-#     pool2 = MaxPooling1D(3)(conv2)
-#     conv3 = Conv1D(128, 3, activation='relu')(pool2)
-    final = GlobalMaxPooling1D()(conv1)  # final = Flatten()(conv1)
-    dens1 = Dense(units=256, activation='relu')(final)
+    pool1 = GlobalMaxPooling1D()(conv1)
+    dens1 = Dense(units=256, activation='relu')(pool1)
     drop2 = Dropout(rate=0.2)(dens1)
     oup = Dense(1, activation='sigmoid')(drop2)
 
     model = Model(inputs=inp, outputs=oup)
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    adam = optimizers.Adam(lr=LEARN_RATE)
+    model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
     return model
+
+
+#
+# f1 score
+#
+def f1_smart(y_true, y_pred):
+    args = np.argsort(y_pred)
+    tp = y_true.sum()
+    fs = (tp - np.cumsum(y_true[args[:-1]])) / np.arange(y_true.shape[0] + tp - 1, tp, -1)
+    res_idx = np.argmax(fs)
+    return 2 * fs[res_idx], (y_pred[args[res_idx]] + y_pred[args[res_idx + 1]]) / 2
 
 
 #
@@ -86,18 +244,23 @@ train_X, valid_X, test_X, train_y, valid_y, word_index, test_id = load_data()
 print("Train length : ", len(train_X))
 print("Valid length : ", len(valid_X))
 print("Test length : ", len(test_X))
-print("Embedding matrix : ", len(word_index))
+# embedding_matrix1 = load_glove(word_index)
+# embedding_matrix3 = load_para(word_index)
+# print("Embedding matrix1 : ", len(embedding_matrix1))
+# print("Embedding matrix3 : ", len(embedding_matrix3))
+# embedding_matrix = np.mean([embedding_matrix1, embedding_matrix3], axis = 0)
+embedding_matrix = np.random.normal(0, 0.01, (min(MAX_FEATURES, len(word_index)), N_EMBED))
+print(embedding_matrix.shape)
 
-model = build_model(len(word_index))
+model = build_model(embedding_matrix)
 
 # trains
 print('='*60)
 for epoch in range(N_EPOCH):
     model.fit(train_X, train_y, epochs=1, batch_size=N_BATCH, verbose=0)
     pred_Y = model.predict(valid_X)
-    pred_Y = pred_Y.reshape(-1) >= THRESHOLD
-    pred_Y = pred_Y.astype(int)
-    print(epoch, ':', f1_score(valid_y, pred_Y, average='macro'))
+    f1, threshold = f1_smart(np.squeeze(valid_y), np.squeeze(pred_Y))
+    print('Epoch: {:2d} F1: {:.4f} at threshold: {:.4f}'.format(epoch, f1, threshold))
 print('='*60)
 
 # predict
@@ -105,5 +268,5 @@ prediction = model.predict(test_X)
 
 # make submission
 prediction = prediction.reshape(-1)
-output = pd.DataFrame(data={"qid": test_id, "prediction": prediction >= THRESHOLD})
+output = pd.DataFrame(data={"qid": test_id, "prediction": prediction > THRESHOLD})
 output.to_csv("submission.csv", index=False, quoting=3)
--- QIQC/src/kaggle_data.py
+++ QIQC/src/kaggle_data.py
@@ -1,4 +1,5 @@
 import codecs
+import re
 
 import numpy as np
 import pandas as pd
@@ -36,11 +37,72 @@ puncts = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&', 
  '“', '★', '”', '–', '●', 'â', '►', '−', '¢', '²', '¬', '░', '¶', '↑', '±', '¿', '▾', '═', '¦', '║', '―', '¥', '▓', '—', '‹', '─', 
  '▒', '：', '¼', '⊕', '▼', '▪', '†', '■', '’', '▀', '¨', '▄', '♫', '☆', 'é', '¯', '♦', '¤', '▲', 'è', '¸', '¾', 'Ã', '⋅', '‘', '∞', 
  '∙', '）', '↓', '、', '│', '（', '»', '，', '♪', '╩', '╚', '³', '・', '╦', '╣', '╔', '╗', '▬', '❤', 'ï', 'Ø', '¹', '≤', '‡', '√', ]
+ 
 def clean_text(x):
-    x = str(x)
+    x = str(x) 
+    for punct in "/-'":
+        x = x.replace(punct, ' ')
+    for punct in '&':
+        x = x.replace(punct, f' {punct} ')
+    for punct in '?!.,"#$%\'()*+-/:;<=>@[\\]^_`{|}~' + '“”’':
+        x = x.replace(punct, '')
     for punct in puncts:
         x = x.replace(punct, f' {punct} ')
     return x
+
+def clean_numbers(x):
+    x = re.sub('[0-9]{5,}', '#####', x)
+    x = re.sub('[0-9]{4,}', '####', x)
+    x = re.sub('[0-9]{3,}', '###', x)
+    x = re.sub('[0-9]{2,}', '##', x)
+    return x
+
+
+#
+# miss spell
+#
+mispell_dict = {"ain't": "is not", "aren't": "are not","can't": "cannot", "'cause": "because", "could've": "could have",
+"couldn't": "could not", "didn't": "did not",  "doesn't": "does not", "don't": "do not", "hadn't": "had not", "hasn't": "has not",
+"haven't": "have not", "he'd": "he would","he'll": "he will", "he's": "he is", "how'd": "how did", "how'd'y": "how do you",
+"how'll": "how will", "how's": "how is",  "I'd": "I would", "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have",
+"I'm": "I am", "I've": "I have", "i'd": "i would", "i'd've": "i would have", "i'll": "i will",  "i'll've": "i will have",
+"i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would", "it'd've": "it would have", "it'll": "it will", 
+"it'll've": "it will have","it's": "it is", "let's": "let us", "ma'am": "madam", "mayn't": "may not", "might've": "might have",
+"mightn't": "might not","mightn't've": "might not have", "must've": "must have", "mustn't": "must not", "mustn't've": "must not have",
+"needn't": "need not", "needn't've": "need not have","o'clock": "of the clock", "oughtn't": "ought not", "oughtn't've": "ought not have",
+"shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have", "she'd": "she would", "she'd've": "she would have",
+"she'll": "she will", "she'll've": "she will have", "she's": "she is", "should've": "should have", "shouldn't": "should not",
+"shouldn't've": "should not have", "so've": "so have","so's": "so as", "this's": "this is","that'd": "that would",
+"that'd've": "that would have", "that's": "that is", "there'd": "there would", "there'd've": "there would have",
+"there's": "there is", "here's": "here is","they'd": "they would", "they'd've": "they would have", "they'll": "they will",
+"they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have", "wasn't": "was not",
+"we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have", "we're": "we are",
+"we've": "we have", "weren't": "were not", "what'll": "what will", "what'll've": "what will have", "what're": "what are",
+"what's": "what is", "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did",
+"where's": "where is", "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is",
+"who've": "who have", "why's": "why is", "why've": "why have", "will've": "will have", "won't": "will not", "won't've": "will not have",
+"would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have", "y'all": "you all", "y'all'd": "you all would",
+"y'all'd've": "you all would have","y'all're": "you all are","y'all've": "you all have","you'd": "you would", "you'd've": "you would have",
+"you'll": "you will", "you'll've": "you will have", "you're": "you are", "you've": "you have", 'colour': 'color', 'centre': 'center',
+'favourite': 'favorite', 'travelling': 'traveling', 'counselling': 'counseling', 'theatre': 'theater', 'cancelled': 'canceled',
+'labour': 'labor', 'organisation': 'organization', 'wwii': 'world war 2', 'citicise': 'criticize', 'youtu ': 'youtube ', 'Qoura': 'Quora',
+'sallary': 'salary', 'Whta': 'What', 'narcisist': 'narcissist', 'howdo': 'how do', 'whatare': 'what are', 'howcan': 'how can',
+'howmuch': 'how much', 'howmany': 'how many', 'whydo': 'why do', 'doI': 'do I', 'theBest': 'the best', 'howdoes': 'how does',
+'mastrubation': 'masturbation', 'mastrubate': 'masturbate', "mastrubating": 'masturbating', 'pennis': 'penis', 'Etherium': 'Ethereum',
+'narcissit': 'narcissist', 'bigdata': 'big data', '2k17': '2017', '2k18': '2018', 'qouta': 'quota', 'exboyfriend': 'ex boyfriend',
+'airhostess': 'air hostess', "whst": 'what', 'watsapp': 'whatsapp', 'demonitisation': 'demonetization', 'demonitization': 'demonetization',
+'demonetisation': 'demonetization'}
+
+def _get_mispell(mispell_dict):
+    mispell_re = re.compile('(%s)' % '|'.join(mispell_dict.keys()))
+    return mispell_dict, mispell_re
+
+mispellings, mispellings_re = _get_mispell(mispell_dict)
+
+def replace_typical_misspell(text):
+    def replace(match):
+        return mispellings[match.group(0)]
+    return mispellings_re.sub(replace, text)
 
 
 #
@@ -54,6 +116,8 @@ def load_data():
     for df in [train_df, test_df]:
         df["question_text"] = df["question_text"].str.lower()
         df["question_text"] = df["question_text"].apply(lambda x: clean_text(x))
+        df["question_text"] = df["question_text"].apply(lambda x: clean_numbers(x))
+        df["question_text"] = df["question_text"].apply(lambda x: replace_typical_misspell(x))
         df["question_text"].fillna("_##_", inplace=True)
 
     # fill up the values
@@ -61,41 +125,13 @@ def load_data():
     test_Text = test_df["question_text"].values
 
     # Tokenize the sentences
-    tokenizer = Tokenizer(oov_token='__UNK__')
-    all_Text = []
-    all_Text.extend(train_Text)
-    all_Text.extend(test_Text)
-    tokenizer.fit_on_texts(list(all_Text))
-    train_X = tokenizer.texts_to_sequences(train_Text)
-    test_X = tokenizer.texts_to_sequences(test_Text)
+    train_tokenizer = Tokenizer()
+    train_tokenizer.fit_on_texts(list(train_Text))
+    
+    test_tokenizer = Tokenizer()
+    test_tokenizer.fit_on_texts(list(test_Text))
 
-    __UNK__ = tokenizer.word_index['__UNK__']
-    count_all = 0
-    count_oov = 0
-    line_all = 0
-    line_oov = 0
-    for test_line in test_X:
-        flag_oov = False
-        for token in test_line:
-            count_all += 1
-            if token == __UNK__:
-                count_oov += 1
-                flag_oov = True
-        line_all += 1
-        if flag_oov:
-            line_oov += 1
-    print(count_oov, '/', count_all)
-    print(line_oov, '/', line_all)
-
-    # Pad the sentences
-    train_X = pad_sequences(train_X, maxlen=MAXLEN)
-    test_X = pad_sequences(test_X, maxlen=MAXLEN)
-
-    # Get the target values
-    train_y = train_df['target'].values
-    test_id = test_df["qid"].values
-
-    return train_X, test_X, train_y, tokenizer.word_index, test_id, train_Text, test_Text
+    return train_tokenizer, test_tokenizer
 
 #
 # check is sring ascii
@@ -103,12 +139,23 @@ def load_data():
 def is_ascii(s):
     return all(ord(c) < 128 for c in s)
 
+
+def f1_smart(y_true, y_pred):
+    args = np.argsort(y_pred)
+    tp = y_true.sum()
+    fs = (tp - np.cumsum(y_true[args[:-1]])) / np.arange(y_true.shape[0] + tp - 1, tp, -1)
+    res_idx = np.argmax(fs)
+    return 2 * fs[res_idx], (y_pred[args[res_idx]] + y_pred[args[res_idx + 1]]) / 2
+
 #
 # main task
 #
-train_X, test_X, train_y, word_index, test_id, train_Text, test_Text = load_data()
-print("Train length : ", len(train_X))
-print("Test length : ", len(test_X))
-print("Dic length : ", len(word_index))
+train_tokenizer, test_tokenizer = load_data()
+print("Train length : ", len(train_tokenizer.word_index))
+print("Test length : ", len(test_tokenizer.word_index))
+
+for token in train_tokenizer.word_index:
+    if not is_ascii(token):
+        print(token)
 
 
