fdb6c060f7d84be0846eb1304c39edfa2472b5e7
paul-hyun <cchyun@gmail.com>
2019-01-13 23:48:46 +0900

    complex 모델 추가

--- QIQC/src/kaggle_complex.py
+++ QIQC/src/kaggle_complex.py
@@ -3,8 +3,11 @@ import numpy as np
 import pandas as pd
 
 from keras import optimizers
+from keras import  backend as K, initializers, regularizers, constraints
 from keras.layers import Dense, Embedding, Input, Dropout, SpatialDropout1D
 from keras.layers import Conv1D, MaxPooling1D, GlobalMaxPooling1D, Flatten
+from keras.layers import Bidirectional, LSTM
+from keras.layers import Layer, concatenate
 from keras.models import Model
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
@@ -140,8 +143,7 @@ def load_data():
     train_y = train_df['target'].values
     test_id = test_df["qid"].values
 
-    train_X, valid_X, train_y, valid_y = train_test_split(train_X, train_y, test_size=0.1, random_state=42, stratify=train_y)
-    return train_X, valid_X, test_X, train_y, valid_y, tokenizer.word_index, test_id
+    return train_X, test_X, train_y, tokenizer.word_index, test_id
 
 
 #
@@ -206,12 +208,86 @@ def load_para(word_index):
 
 
 #
-# build model
+# Attention
 #
-def build_model(embedding_matrix):
+# https://www.kaggle.com/qqgeogor/keras-lstm-attention-glove840b-lb-0-043
+class Attention(Layer):
+    def __init__(self, step_dim,
+                 W_regularizer=None, b_regularizer=None,
+                 W_constraint=None, b_constraint=None,
+                 bias=True, **kwargs):
+        self.supports_masking = True
+        self.init = initializers.get('glorot_uniform')
+
+        self.W_regularizer = regularizers.get(W_regularizer)
+        self.b_regularizer = regularizers.get(b_regularizer)
+
+        self.W_constraint = constraints.get(W_constraint)
+        self.b_constraint = constraints.get(b_constraint)
+
+        self.bias = bias
+        self.step_dim = step_dim
+        self.features_dim = 0
+        super(Attention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        assert len(input_shape) == 3
+
+        self.W = self.add_weight((input_shape[-1],),
+                                 initializer=self.init,
+                                 name='{}_W'.format(self.name),
+                                 regularizer=self.W_regularizer,
+                                 constraint=self.W_constraint)
+        self.features_dim = input_shape[-1]
+
+        if self.bias:
+            self.b = self.add_weight((input_shape[1],),
+                                     initializer='zero',
+                                     name='{}_b'.format(self.name),
+                                     regularizer=self.b_regularizer,
+                                     constraint=self.b_constraint)
+        else:
+            self.b = None
+
+        self.built = True
+
+    def compute_mask(self, input, input_mask=None):
+        return None
+
+    def call(self, x, mask=None):
+        features_dim = self.features_dim
+        step_dim = self.step_dim
+
+        eij = K.reshape(K.dot(K.reshape(x, (-1, features_dim)),
+                        K.reshape(self.W, (features_dim, 1))), (-1, step_dim))
+
+        if self.bias:
+            eij += self.b
+
+        eij = K.tanh(eij)
+
+        a = K.exp(eij)
+
+        if mask is not None:
+            a *= K.cast(mask, K.floatx())
+
+        a /= K.cast(K.sum(a, axis=1, keepdims=True) + K.epsilon(), K.floatx())
+
+        a = K.expand_dims(a)
+        weighted_input = x * a
+        return K.sum(weighted_input, axis=1)
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0],  self.features_dim
+
+
+#
+# build cnn model
+#
+def build_cnn(embedding_matrix):
     inp = Input(shape=(MAXLEN,), dtype='float64')
     input_embedding = Embedding(input_dim=embedding_matrix.shape[0], output_dim=embedding_matrix.shape[1], weights=[embedding_matrix], input_length=MAXLEN, trainable=TRAIN_EMBED)(inp)
-   
+    
     drop1 = SpatialDropout1D(0.2)(input_embedding)
     conv1 = Conv1D(128, 3, activation='relu')(drop1)
     pool1 = GlobalMaxPooling1D()(conv1)
@@ -224,42 +300,122 @@ def build_model(embedding_matrix):
     model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
     return model
 
+#
+# build lstm model
+#
+def build_lstm(embedding_matrix):
+    inp = Input(shape=(MAXLEN,), dtype='float64')
+    input_embedding = Embedding(input_dim=embedding_matrix.shape[0], output_dim=embedding_matrix.shape[1], weights=[embedding_matrix], input_length=MAXLEN, trainable=TRAIN_EMBED)(inp)
+    
+    drop1 = SpatialDropout1D(0.2)(input_embedding)
+    lstm1 = Bidirectional(LSTM(128))(drop1)
+    dens1 = Dense(units=256, activation='relu')(lstm1)
+    drop2 = Dropout(rate=0.2)(dens1)
+    oup = Dense(1, activation='sigmoid')(drop2)
+
+    model = Model(inputs=inp, outputs=oup)
+    adam = optimizers.Adam(lr=LEARN_RATE)
+    model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+
+#
+# build attention model
+#
+def build_attention(embedding_matrix):
+    inp = Input(shape=(MAXLEN,), dtype='float64')
+    input_embedding = Embedding(input_dim=embedding_matrix.shape[0], output_dim=embedding_matrix.shape[1], weights=[embedding_matrix], input_length=MAXLEN, trainable=TRAIN_EMBED)(inp)
+    
+    drop1 = SpatialDropout1D(0.2)(input_embedding)
+    lstm1 = Bidirectional(LSTM(128, return_sequences=True))(drop1)
+    conv1 = Conv1D(128, 3, padding='same', activation='relu')(drop1)
+    atten1 = Attention(MAXLEN)(lstm1)
+    atten2 = Attention(MAXLEN)(conv1)
+    max_pool = GlobalMaxPooling1D()(drop1)
+    conc1 = concatenate([atten1, atten2, max_pool])
+    dens1 = Dense(units=256, activation='relu')(conc1)
+    drop2 = Dropout(rate=0.2)(dens1)
+    oup = Dense(1, activation='sigmoid')(drop2)
+
+    model = Model(inputs=inp, outputs=oup)
+    adam = optimizers.Adam(lr=LEARN_RATE)
+    model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
 
 #
 # main task
 #
-train_X, valid_X, test_X, train_y, valid_y, word_index, test_id = load_data()
+train_X, test_X, train_y, word_index, test_id = load_data()
 print("Train length : ", len(train_X))
-print("Valid length : ", len(valid_X))
 print("Test length : ", len(test_X))
 # embedding_matrix1 = load_glove(word_index)
+# print("Embedding glove : ", embedding_matrix1.shape)
+# embedding_matrix2 = load_fasttext(word_index)
+# print("Embedding fasttext : ", embedding_matrix2.shape)
 # embedding_matrix3 = load_para(word_index)
-# print("Embedding matrix1 : ", len(embedding_matrix1))
-# print("Embedding matrix3 : ", len(embedding_matrix3))
-# embedding_matrix = np.mean([embedding_matrix1, embedding_matrix3], axis = 0)
+# print("Embedding para : ", embedding_matrix3.shape)
+# embedding_matrix = np.mean([embedding_matrix1, embedding_matrix2, embedding_matrix3], axis = 0)
+# embedding_matrix = np.concatenate((embedding_matrix1, embedding_matrix2, embedding_matrix3), axis=1)
 embedding_matrix = np.random.normal(0, 0.01, (min(MAX_FEATURES, len(word_index) + 1), N_EMBED))
-print(embedding_matrix.shape)
+print("Embedding matrix : ", embedding_matrix.shape)
 
-model = build_model(embedding_matrix)
+models = []
+models.append({'name': 'cnn', 'model':build_cnn(embedding_matrix), 'epoch': 10})
+models.append({'name': 'rnn', 'model':build_lstm(embedding_matrix), 'epoch': 10})
+# models.append({'name': 'attention', 'model':build_attention(embedding_matrix), 'epoch': 10})
 
-# trains
-print('='*60)
-threshold = 0.33
-for epoch in range(N_EPOCH):
-    model.fit(train_X, train_y, epochs=1, batch_size=N_BATCH, verbose=0)
-    pred_Y = model.predict(valid_X)
-    f1_val = 0.0
-    for t in np.arange(0.25, 0.75, 0.01):
-        f1 = metrics.f1_score(valid_y, (pred_Y > t).astype(int))
-        if f1_val < f1:
-            f1_val, threshold = f1, t
-    print('Epoch: {:3d} F1: {:.4f} at threshold: {:.4f}'.format(epoch, f1_val, threshold))
-print('='*60)
 
-# predict
-prediction = model.predict(test_X)
+#
+# train
+#
+def train(model, epochs, train_X, train_y):
+    max_epoch = 0
+    max_f1 = 0.0
+    max_th = 0.0
+    max_prediction = None
 
-# make submission
-prediction = prediction.reshape(-1)
-output = pd.DataFrame(data={"qid": test_id, "prediction": prediction > threshold})
-output.to_csv("submission.csv", index=False, quoting=3)
+    train_X, valid_X, train_y, valid_y = train_test_split(train_X, train_y, test_size=0.05, random_state=42, stratify=train_y)
+    for epoch in range(epochs):
+        model.fit(train_X, train_y, epochs=1, batch_size=N_BATCH, verbose=0)
+        pred_Y = model.predict(valid_X)
+        f1_val = 0.0
+        th_val = 0.0
+        for th in np.arange(0.25, 0.75, 0.01):
+            f1 = metrics.f1_score(valid_y, (pred_Y > th).astype(int))
+            if f1_val < f1:
+                f1_val, th_val = f1, th
+        print('\tEpoch: {:2d}, F1: {:.4f}, Threshold: {:.4f}'.format(epoch, f1_val, th_val))
+        if max_f1 < f1_val:
+            max_epoch = epoch
+            max_f1 = f1_val
+            max_th = th_val
+            max_prediction = (model.predict(test_X).reshape(-1) > th_val).astype(int)
+            print('\t\tMax f1, make prediction')
+    return max_epoch, max_f1, max_th, max_prediction
+
+
+max_values = []
+for item in models:
+    print('='*60)
+    print(item)
+    epoch, f1, threshold, prediction = train(item['model'], item['epoch'], train_X, train_y)
+    max_values.append({'item': item, 'epoch': epoch, 'f1': f1, 'threshold': threshold, 'prediction': prediction})
+    print('='*60)
+
+
+#
+# submission max prediction
+#
+def submission_max(test_id, max_values):
+    max_value = max_values[0]
+    for vvv in max_values:
+        if max_value['f1'] < vvv['f1']:
+            max_value = vvv
+    print('Save submission.csv Name: {}, Epoch: {:2d}, F1: {:.4f}, Threshold: {:.4f}'.format(max_value['item']['name'], max_value['epoch'], max_value['f1'], max_value['threshold']))
+    output = pd.DataFrame(data={"qid": test_id, "prediction": max_value['prediction'] > 0.5})
+    output.to_csv("submission.csv", index=False, quoting=3)
+
+
+submission_max(test_id, max_values)
+
